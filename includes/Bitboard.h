cbd1a4f8bafff0b3961bbfc47b321eedfbb636e7
mefisher95 <mefisher2@cougars.ccis.edu>
2021-01-17 20:43:33 -0600

    2021-01-17 20:43:33.031326

---
+++ includes/Bitboard.h
@@ -0,0 +1,18 @@
+#ifndef BITBOARD_H
+#define BITBOARD_H
+
+#include
+
+class Bitboard
+{
+public:
+    Bitboard()
+    : player0(0), player1(0)
+    {}
+
+private:
+    uint64_t player0;
+    uint64_t player1;
+};
+
+#endif
